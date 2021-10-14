11d345da91376b9cbae6c69265aa95aaa041bdf4
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2021-10-14 14:15:31 -0300

    feat: minimal react pwa

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" width="200px" height="200px"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
